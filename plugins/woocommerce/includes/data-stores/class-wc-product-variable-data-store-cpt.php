f9be926f45af9cee670d03381ac9af35b3931ddf
kalessil <kalessil@gmail.com>
2026-07-16 06:43:47 +0200

    Performance: audit the variable product datastore and apply additional micro-optimizations.

--- plugins/woocommerce/includes/data-stores/class-wc-product-variable-data-store-cpt.php
+++ plugins/woocommerce/includes/data-stores/class-wc-product-variable-data-store-cpt.php
@@ -356,7 +356,7 @@ class WC_Product_Variable_Data_Store_CPT extends WC_Product_Data_Store_CPT imple
 			// If the prices are not stored for this hash, generate them and add to the transient.
 			// Check also the opposite price hash as it may have changed (see get_price_hash).
 			if ( empty( $transient_cached_prices_array[ $price_hash ] ) ||
-				( ! is_null( $opposite_price_hash ) && ( $opposite_price_hash !== $price_hash ) && empty( $transient_cached_prices_array[ $opposite_price_hash ] ) ) ) {
+				( $opposite_price_hash && $opposite_price_hash !== $price_hash && empty( $transient_cached_prices_array[ $opposite_price_hash ] ) ) ) {
 				$prices_array = array(
 					'price'         => array(),
 					'regular_price' => array(),
@@ -531,7 +531,7 @@ class WC_Product_Variable_Data_Store_CPT extends WC_Product_Data_Store_CPT imple
 				// Add all pricing data to the transient array.
 				foreach ( $prices_array as $key => $values ) {
 					$transient_cached_prices_array[ $price_hash ][ $key ] = $values;
-					if ( ! is_null( $opposite_price_hash ) && $opposite_price_hash !== $price_hash ) {
+					if ( $opposite_price_hash && $opposite_price_hash !== $price_hash ) {
 						$transient_cached_prices_array[ $opposite_price_hash ][ $key ] = $values;
 					}
 				}
