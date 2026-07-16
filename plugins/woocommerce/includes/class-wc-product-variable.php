28ce58e31893cf6f23071852fe3e54ddf169d348
kalessil <kalessil@gmail.com>
2026-07-16 11:11:05 +0200

    Performance: audit the variable product datastore and apply additional micro-optimizations.

--- plugins/woocommerce/includes/class-wc-product-variable.php
+++ plugins/woocommerce/includes/class-wc-product-variable.php
@@ -45,7 +45,10 @@ class WC_Product_Variable extends WC_Product {
 	 *
 	 * @var array<string,array<string,array<int,float>>>
 	 */
-	private array $variation_prices = array();
+	private array $variation_prices = array(
+		'for_display:0' => array(),
+		'for_display:1' => array()
+	);
 
 	/**
 	 * Get internal type.
@@ -108,13 +111,11 @@ class WC_Product_Variable extends WC_Product {
 		$prices = $this->data_store->read_price_data( $this, $for_display );
 
 		// Performance note: weak comparison identifies (current data shape only) changes while skipping repetitive sorting.
-		$cache_key     = $for_display ? '1' : '0';
-		$cached_prices = $this->variation_prices[ $cache_key ] ?? array();
-		if ( $cached_prices != $prices ) { // phpcs:ignore Universal.Operators.StrictComparisons.LooseNotEqual
-			foreach ( $prices as $price_key => $variation_prices ) {
-				$prices[ $price_key ] = $this->sort_variation_prices( $variation_prices );
+		$cache_key = $for_display ? 'for_display:1' : 'for_display:0';
+		if ( $this->variation_prices[ $cache_key ] != $prices ) { // phpcs:ignore Universal.Operators.StrictComparisons.LooseNotEqual
+			foreach ( $prices as $variation_prices ) {
+				$this->variation_prices[ $cache_key ] = $this->sort_variation_prices( $variation_prices );
 			}
-			$this->variation_prices[ $cache_key ] = $prices;
 		}
 
 		return $this->variation_prices[ $cache_key ];
